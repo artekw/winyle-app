21772e5cb61509a4779942b6454605c3d080950e
Artur Wronowski <arteqw@gmal.com>
2015-05-23 23:00:40 +0200

    Modyfikacja migracji

--- vinyl/migrations/0006_auto_20150430_1700.py
+++ vinyl/migrations/0006_auto_20150430_1700.py
@@ -14,7 +14,6 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Artist',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=100, verbose_name=b'Wykonawca/Zesp\xc3\xb3\xc5\x82')),
             ],
         ),
